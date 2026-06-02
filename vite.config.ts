df2cafc004679f9bbfe55f9c05457fc433430c6a
anmobd111 <sanmobd111@gmail.com>
2026-06-02 17:12:18 +0600

    install nitro

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
+import { nitro } from 'nitro/vite';
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [tailwindcss(), nitro()],
   tanstackStart: {
     server: { entry: "server" }
   }
